7aece5b5221f7a98f54a111799621ea5ba01e7cb
youyou.dyy <youyou.dyy@alibaba-inc.com>
2020-07-23 17:05:42 +0800

    编译多目录工程

---
+++ sample2/src/hello/hello.c
@@ -0,0 +1,8 @@
+//
+// Created by Timmy on 2020/7/23.
+//
+#include <stdio.h>
+
+void printHello(void) {
+    printf("hello");
+}
---
+++ sample2/src/main.c
@@ -0,0 +1,13 @@
+//
+// Created by Timmy on 2020/7/23.
+//
+#include "hello/hello.h"
+#include "world/world.h"
+
+#include <stdio.h>
+
+int main() {
+    printHello();
+    printf(" ");
+    printWorld();
+}
